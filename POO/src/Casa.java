fec69236ffb5de26a0e8ad954a6bac18456110b5
Tiago Mota <tmota.seg@gmail.com>
2026-09-16 20:54:34 -0300

    Feat: Classe casa com o atributo inteligente, que será manipulado pelo método 'acenderLuz'.

---
+++ POO/src/Casa.java
@@ -0,0 +1,60 @@
+public class Casa {
+    boolean inteligente;
+
+    void acenderLuz() {
+        if (inteligente)
+            System.out.println("Comando de voz");
+        else
+            System.out.println("Pressionar interruptor");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

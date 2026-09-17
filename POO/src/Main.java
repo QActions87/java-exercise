51eed2f267defbd5a50a90102a15a6deef50a9cf
Tiago Mota <tmota.seg@gmail.com>
2026-09-16 21:19:51 -0300

    Feat: A partir da classe Casa, com seu método 'acenderLuz' que manipula o seu atributo 'inteligente', criei a classe Main que recebe o entry point (classe main), para instância dois objetos do tipo da classe 'Casa' (casa1 e casa2), acessar o atributo 'inteligente' e o modificando em 'false' e 'true' - respectivamente -, depois chamar o método acenderLuz.

---
+++ POO/src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        Casa casa1 = new Casa();
+        Casa casa2 = new Casa();
+
+        casa1.inteligente = false;
+        casa2.inteligente = true;
+
+        casa1.acenderLuz();
+        casa2.acenderLuz();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

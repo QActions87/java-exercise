ae8b0bc40fb85cba52ea21e9390f7e9aa8378e11
Tiago Mota <tmota.seg@gmail.com>
2026-09-16 22:42:03 -0300

    Feat: Criação do construtor padrão. Pois, depois que crei o construtor com parâmetro, perdi o acesso direto ao construtor padrão ao instanciar um objeto. Agora, posso instanciar o objeto 'casa3'.

--- POO/src/Casa.java
+++ POO/src/Casa.java
@@ -1,13 +1,16 @@
 public class Casa {
     // Atributo:
     boolean inteligente;
+    // Construtor padrão (sem parâmetro):
+    public Casa() {
+    }
 
     // Construtor com parâmetro:
     public Casa(boolean inteligente) {
         this.inteligente = inteligente;
     }
 
-
+    // Metodo:
     void acenderLuz() {
         if (inteligente)
             System.out.println("Comando de voz");
--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Casa casa1 = new Casa();
-        Casa casa2 = new Casa();
+        Casa casa1 = new Casa(false);
+        Casa casa2 = new Casa(true);
 
-        casa1.inteligente = false;
-        casa2.inteligente = true;
+//        casa1.inteligente = false;
+//        casa2.inteligente = true;
 
         casa1.acenderLuz();
         casa2.acenderLuz();

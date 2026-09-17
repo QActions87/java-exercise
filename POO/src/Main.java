2ab374c2af588a9c208e5553ba14f8ecd54dce90
Tiago Mota <tmota.seg@gmail.com>
2026-09-16 22:53:52 -0300

    Feat: Criei o objeto casa3 com o constrrutor padrão (SEM parâmetrro), para acessar o atributo 'inteligente' por meio dele e setar o valor booleano 'false'.

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,13 +1,19 @@
 public class Main {
     public static void main(String[] args) {
+        // Instâncias criadas com o construtor com parâmetro:
         Casa casa1 = new Casa(false);
         Casa casa2 = new Casa(true);
+        // Instância criada com o construtor padrão:
+        Casa casa3 = new Casa();
 
-//        casa1.inteligente = false;
-//        casa2.inteligente = true;
+        // Modificando o atributo a partir dos objetos criados com o
+        // construtor padrão:
+        // casa1.inteligente = false;
+        // casa2.inteligente = true;
 
         casa1.acenderLuz();
         casa2.acenderLuz();
+        casa3.inteligente = false;
     }
 }
 

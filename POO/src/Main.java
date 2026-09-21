d9def464314fd4103d3c9ef80d11c59a60c2ddcc
Tiago Mota <tmota.seg@gmail.com>
2026-09-21 00:24:51 -0300

    Feat: ENCAPSULAMENTO: Acessando os método e atributos (diretamente sem getters) da classe, a partir da instância carro da classe Carro.

--- POO/src/Main.java
+++ POO/src/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-
+        // Instanciando o objeto carro:
         Carro carro = new Carro();
-
+        // Acessando os métodos a partir do objeto carro:
         carro.ligarCarro();
         carro.acelerar();
         carro.frear();
+
+        // Acessando os atributos diretamente:
+        System.out.println(carro.motor);
+        System.out.println(carro.volante);
+        System.out.println(carro.airbag);
     }
 }
